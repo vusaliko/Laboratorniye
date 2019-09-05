a0d36f53e77791c7d79b2d9d2cb97d7fd4f37844
vusaliko <31347556+vusaliko@users.noreply.github.com>
2019-09-05 21:57:06 +0300

    Update Лабораторная 1.4.py

--- Лабораторная 1.4.py
+++ Лабораторная 1.4.py
@@ -1,10 +1,10 @@
 while True: #Цикл
     print("Введите 2 ненулевых числа")
-    a=float(input())
-    b=float(input())
-    print("Сумма=" + str(a**2+b**2))
-    print("a^2-b^2=" + str(a**2-b**2))
-    print("b^2-a^2=" + str(b**2-a**2))
-    print("Произведение=" + str((a**2)*b**2))
-    print("a^2/b^2=" + str(a**2/b**2))
-    print("b^2/a^2=" + str(b**2/a**2))
+    a=float(input())**2
+    b=float(input())**2
+    print("Сумма=" + str(a+b))
+    print("a^2-b^2=" + str(a-b))
+    print("b^2-a^2=" + str(b-a))
+    print("Произведение=" + str(a*b))
+    print("a^2/b^2=" + str(a/b))
+    print("b^2/a^2=" + str(b/a))
