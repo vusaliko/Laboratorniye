d07a965bb7d58173efaa5b172ec2978c3959df80
vusaliko <31347556+vusaliko@users.noreply.github.com>
2019-09-09 22:38:35 +0300

    Update Лабораторная 2.1.py

--- Лабораторная 2.1.py
+++ Лабораторная 2.1.py
@@ -8,4 +8,4 @@ while True:
     x2=float(input())
     print("Введите y точки 2")
     y2=float(input())
-    print("Длина=" + str(math.sqrt((x1-x2)**2+(y1-y2)**2)))
+    print("Расстояние=" + str(math.sqrt((x1-x2)**2+(y1-y2)**2)))
